db5a39e3b716ac57b89371135d6c2351c09be641
IAmBin <13424029717@163.com>
2020-07-12 16:05:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+##uni-app三端复制插件
+uni-app的复制插件，复制字符串到剪贴板，兼容三端 小程序、App、H5，试过所有三端复制插件，这个是兼容性最好的，如有不兼容的情况，请留言
+
+
+###使用方法
+```
+// 引入文件
+ import uniCopy from '@/js_sdk/uni-copy/uni-copy.js'
+ 
+ export default {
+ 
+ 	methods: {
+    // 触发方法
+     copy() {
+       let content = 'uni复制插件'
+	   content = typeof content === 'string' ? content : content.toString() // 复制内容，必须字符串，数字需要转换为字符串
+       const result = uniCopy(content)
+       if (result === false) {
+       	uni.showToast({
+       		title: '不支持',
+       	})
+       } else {
+       	uni.showToast({
+       		title: '复制成功',
+       		icon: 'none'
+       	})
+       }
+     }
+ 	}
+ }
+ ```
---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<view class="desciption">
+			<view>uni-app的复制插件</view>
+			<view>支持 H5、小程序、App 亲测可信</view>
+			<view>如有不兼容的情况，请留言</view>
+		</view>
+		<view class="text-area">
+			<button type="primary" @click="copy">复制</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniCopy from '@/js_sdk/uni-copy/uni-copy.js'
+
+	export default {
+		methods: {
+			copy() {
+				let content = 'uni复制插件' 
+				content = typeof content === 'string' ? content : content.toString() // 复制内容，必须字符串，数字需要转换为字符串
+				const result = uniCopy(content)
+				if (result === false) {
+					uni.showToast({
+						title: '不支持',
+					})
+				} else {
+					uni.showToast({
+						title: '复制成功',
+						icon: 'none'
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		max-width: 690px;
+		margin: 10px auto;
+		font-size: 24px;
+		padding: 10px;
+	}
+
+	.desciption {
+		margin: 20px auto;
+		line-height: 1.8em;
+	}
+</style>
